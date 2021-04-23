17d23ee1c4510dd5b7f736adca2d05603ee0fdb6
Michael McCune <elmiko@redhat.com>
2021-04-23 14:26:54 -0400

    add a little logic to detect target man page

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("What manual page do you want?")
-	fmt.Println("For example, try 'oc man index'")
+	if len(os.Args) == 1 {
+		fmt.Println("What manual page do you want?")
+		fmt.Println("For example, try 'oc man index'")
+	} else {
+		target := os.Args[1]
+		if target == "index" {
+			fmt.Println("Printing index...")
+		} else {
+			fmt.Println("I don't have a page for", target)
+		}
+	}
 }
