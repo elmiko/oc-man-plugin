ffee5f01c87dfce95e082b03036c74033473e0a1
Michael McCune <elmiko@redhat.com>
2021-04-23 15:59:42 -0400

    adding basic topics processing

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+.PHONY: clean tidy vendor
+
 build:
 	go build -o bin/oc-man
 
 clean:
 	rm bin/*
+
+tidy:
+	go mod tidy
+
+vendor: tidy
+	go mod vendor
--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	_ "embed"
+
 	"fmt"
+	"log"
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
+
+//go:embed topics.yaml
+var topicsBytes []byte
+
+type TopicsEntry struct {
+	Title    string `yaml:"title"`
+	Location string `yaml:"location"`
+}
+
+type TopicsList struct {
+	Topics []TopicsEntry `yaml:"topics"`
+}
+
+func getTopics() TopicsList {
+	var topics TopicsList
+
+	if err := yaml.Unmarshal(topicsBytes, &topics); err != nil {
+		log.Fatalf("cannot unmarshal topics data: %v", err)
+	}
+
+	return topics
+}
 
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("What manual page do you want?")
-		fmt.Println("For example, try 'oc man index'")
+		fmt.Println("For example, try 'oc man topics'")
 	} else {
+		topics := getTopics()
 		target := os.Args[1]
-		if target == "index" {
-			fmt.Println("Printing index...")
+		if target == "topics" {
+			fmt.Println("I know about the following topics:")
+			for _, t := range topics.Topics {
+				fmt.Println(t.Title)
+			}
 		} else {
 			fmt.Println("I don't have a page for", target)
 		}
